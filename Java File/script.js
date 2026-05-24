426195d8bb3fca05723c750ffb1a8a7292621b61
ASHOKPENDRA <ashokpendra989@gmail.com>
2026-05-24 16:12:26 +0530

    Add files via upload

---
+++ Java File/script.js
@@ -0,0 +1,17 @@
+const darkModeToggle = document.getElementById("darkModeToggle");
+
+// Check if user previously enabled dark mode
+if (localStorage.getItem("darkMode") === "enabled") {
+  document.body.classList.add("dark-mode");
+  darkModeToggle.checked = true;
+}
+
+darkModeToggle.addEventListener("change", () => {
+  if (darkModeToggle.checked) {
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("darkMode", "disabled");
+  }
+}); 
